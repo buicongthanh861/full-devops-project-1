16eaac0e4894a34a0bad84acf9f2750efd3672bf
buicongthanh861 <buicongthanh861@gmail.com>
2025-09-11 15:23:54 +0700

    add ansible playbook

---
+++ ansible/setup.md
@@ -0,0 +1,36 @@
+Setup Ansible
+Install ansibe on Ubuntu 22.04
+
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+Add Jenkins master and slave as hosts Add jenkins master and slave private IPs in the inventory file in this case, we are using /opt is our working directory for Ansible.
+
+1. lay file key da dowload o tu aws ve xong tha vao server ansible
+1.1 cd/home/ubuntu de lay key.pem sau do di chuyen file nay vao /opt bang lenh mv key.pem /opt. Dat quyen cho file cho dung la chmod 44 /opt/congthanh-devops
+1.2 vao ls kiem tra xem co file key.pem chua sau do viet file hosts de lien ket cac server
+
+------------noi dung file hosts------------------
+[jenkins-master]
+10.1.1.192
+
+[jenkins-master:vars]
+ansible_user=ubuntu
+ansible_ssh_private_key_file=/opt/congthanh-devops.pem
+
+[jenkins-slave]
+
+
+[jenkins-slave:vars]
+ansible_user=ubuntu
+ansible_ssh_private_key_file=/opt/congthanh-devops.pem
+-----------------------------------------------------
+
+Test the connection:
+ansible -i hosts all -m ping
+neu thay nhu nay la da thanh cong
+10.1.1.192 | SUCCESS => {
+    "changed": false,
+    "ping": "pong"
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,13 +11,115 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "demo-server" {
-    ami = "ami-0779c82fbb81e731c"
-    instance_type = "t3.micro"
-    key_name = "congthanh-devops"
+# Security Group
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "Allow SSH, HTTP, HTTPS"
+  vpc_id = aws_vpc.congthanh-vpc.id
 
-    tags = {
-      Name = "demo-server"
-    }
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "demo-sg"
+  }
 }
+
+resource "aws_vpc" "congthanh-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "congthanh-vpc"
+  }
+}
+
+resource "aws_subnet" "congthanh-public-subnet-01" {
+  vpc_id            = aws_vpc.congthanh-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "ap-southeast-1a"
+  tags = {
+    Name = "congthanh-public-subnet-01" 
+  }
+}
+
+resource "aws_subnet" "congthanh-public-subnet-02" {
+  vpc_id            = aws_vpc.congthanh-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-southeast-1b"
+  tags = {
+    Name = "congthanh-public-subnet-02" 
+  }
+}
+
+resource "aws_internet_gateway" "congthanh-igw" {
+  vpc_id = aws_vpc.congthanh-vpc.id
+  tags = {
+    Name = "congthanh-igw"
+  }
+}
+
+resource "aws_route_table" "congthanh-public-rt" {
+  vpc_id = aws_vpc.congthanh-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.congthanh-igw.id
+  } 
+}
+
+resource "aws_route_table_association" "congthanh-rta-public-subnet-01" {
+  subnet_id      = aws_subnet.congthanh-public-subnet-01.id
+  route_table_id = aws_route_table.congthanh-public-rt.id
+  
+}
+
+
+resource "aws_route_table_association" "congthanh-rta-public-subnet-02" {
+  subnet_id      = aws_subnet.congthanh-public-subnet-02.id
+  route_table_id = aws_route_table.congthanh-public-rt.id
+  
+}
+
+# EC2 Instance
+resource "aws_instance" "demo-server" {
+  ami                    = "ami-0933f1385008d33c4"
+  instance_type          = "t3.micro"
+  key_name               = "congthanh-devops"
+  subnet_id = aws_subnet.congthanh-public-subnet-01.id
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  for_each =  toset(["Jenkins-master", "build-slave", "ansible"])
+  tags = {
+    Name = each.key
+  }
+}
+
 
